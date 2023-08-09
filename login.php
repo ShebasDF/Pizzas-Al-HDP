1fedbef121f5f86ac6902507489412a1774a3b0e
Sebastian Diaz <diazsebastian983@gmail.com>
2023-08-09 19:52:48 -0300

    confirmacion de usuario

--- login.php
+++ login.php
@@ -1,6 +1,10 @@
 <?php
 $usr=$_POST["username"];
 $pass=$_POST["password"];
-echo $usr;
-echo $pass;
+if(((strcmp($usr,"maria")==0)||(strcmp($usr,"maria@twitter.com"))==0)&&(strcmp($pass,"123456")==0)){echo "Hola Bienvenido";}
+else{
+    echo " ERROR----a donde vas----?";
+
+}
 ?>
+
