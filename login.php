0c94b33b5d217a3e23b391d3dba05c0efddde219
Sebastian Diaz <diazsebastian983@gmail.com>
2023-09-04 20:54:12 -0300

    login.phpv2

--- login.php
+++ login.php
@@ -1,7 +1,10 @@
 <?php
 include_once("config_login.php");
+try{
 $PDO = new PDO("mysql:host =".SERVER_NAME.";dbname =".DATABASE_NAME,USER_NAME, PASSWORD);
 echo "conexion exitosa";
-
+}
+catch(PDOException $e) {
+    echo "Connection Interrumpida: " . $e->getMessage();}
 ?>
 
