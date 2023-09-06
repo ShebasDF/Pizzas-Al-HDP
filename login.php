f156dfa20f9e06156f10bc1b3dc67eb2aed8687f
Sebastian Diaz <diazsebastian983@gmail.com>
2023-09-06 20:58:10 -0300

    Login.php/objetos

--- login.php
+++ login.php
@@ -1,10 +1,20 @@
 <?php
 include_once("config_login.php");
-try{
-$PDO = new PDO("mysql:host =".SERVER_NAME.";dbname =".DATABASE_NAME,USER_NAME, PASSWORD);
-echo "conexion exitosa";
+try {
+    $pdo = new PDO("mysql:host=" . SERVER_NAME . ";dbname=" . DATABASE_NAME, USER_NAME, PASSWORD);
+    // set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
-catch(PDOException $e) {
-    echo "Connection Interrumpida: " . $e->getMessage();}
-?>
 
+$usr = $_POST["username"];  
+$pass = $_POST["password"];
+$hased_pass = hash("sha256",$pass);
+$sql = "SELECT * FROM users WHERE ((username=:usr) OR (email=:usr)) AND (PASSWORD=:hashed_pass) AND (active="si")";
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(":usr,$usr");
+$stmt->bindValue(":hashed_pass,$hashed_pass");
+$stmt->execute();
+//PDO=interfaz que se usa para ingresar a base de dato (distintas base de datos)?>
